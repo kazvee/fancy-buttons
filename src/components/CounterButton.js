9b97712aeeb44b17d0985b6ad1a6bd96da1b899f
kazvee <kayvee123@gmail.com>
2023-09-04 17:08:08 -0600

    Implement useState on CounterButton component

--- src/components/CounterButton.js
+++ src/components/CounterButton.js
@@ -1,5 +1,9 @@
-const CounterButton = () => {
-  return <button className='CounterButton'>You clicked me ❎ times!</button>;
+import { useState } from "react";
+
+const CounterButton = (props) => {
+  const [clickAmount, setClickAmount] = useState(0);
+
+  return <button className='CounterButton'>You clicked me {clickAmount} times!</button>;
 };
 
 export default CounterButton;
