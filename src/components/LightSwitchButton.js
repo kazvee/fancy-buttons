647cd9d84aec2c328ea5a255c4bfb991ebf8abc0
kazvee <kayvee123@gmail.com>
2023-09-04 17:45:50 -0600

    Implement useState on LightSwitchButton component

--- src/components/LightSwitchButton.js
+++ src/components/LightSwitchButton.js
@@ -1,14 +1,12 @@
-const LightSwitchButton = () => {
+import { useState } from 'react';
+
+const LightSwitchButton = (props) => {
+  const [light, setLight] = useState('off');
+
   return (
     <button className='LightSwitchButton'>
-      {/* When the state is on */}
-      <span>
-        <i>💡</i> I'm on!
-      </span>
-      {/* When the state is off */}
-      <span className='off'>
-        <i>💡</i> I'm off!
-      </span>
+      {light === 'on' && <span><i>💡</i> I'm on!</span>}
+      {light === 'off' && <span className='off'><i>💡</i> I'm off!</span>}
     </button>
   );
 };
