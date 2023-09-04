20e30626d2334ec5714cf885e3c25555ceb102b5
kazvee <kayvee123@gmail.com>
2023-09-04 13:04:04 -0600

    Import all necessary components into App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
 import './App.css';
+import AngryButton from '../src/components/AngryButton';
+import CounterButton from '../src/components/CounterButton';
+import LightSwitchButton from '../src/components/LightSwitchButton';
+import TextRepeaterButton from '../src/components/TextRepeaterButton';
 
-function App() {
-  return <div className='App'></div>;
-}
+const App = () => {
+  return (
+    <div className={`App`}>
+      <h1>Fancy Buttons! 😃</h1>
+      <section>
+        <AngryButton />
+        <CounterButton />
+        <LightSwitchButton />
+        <TextRepeaterButton />
+      </section>
+    </div>
+  );
+};
 
 export default App;
