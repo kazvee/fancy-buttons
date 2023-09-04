e0c394968b8429d6b1ddeb8e5d50a2eb43a48d4f
kazvee <kayvee123@gmail.com>
2023-09-04 12:05:31 -0600

    Create TextRepeaterButton component

---
+++ src/components/TextRepeaterButton.js
@@ -0,0 +1,9 @@
+const TextRepeaterButton = () => {
+  return (
+    <button className='TextRepeaterButton'>
+      <span>I like this text! 🤩</span>
+    </button>
+  );
+};
+
+export default TextRepeaterButton;
