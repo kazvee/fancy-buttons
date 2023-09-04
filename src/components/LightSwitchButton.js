54ff5f2f1b04a50e9527f746569252637f57b9c0
kazvee <kayvee123@gmail.com>
2023-09-04 12:00:32 -0600

    Create LightSwitchButton component

---
+++ src/components/LightSwitchButton.js
@@ -0,0 +1,16 @@
+const LightSwitchButton = () => {
+  return (
+    <button className='LightSwitchButton'>
+      {/* When the state is on */}
+      <span>
+        <i>💡</i> I'm on!
+      </span>
+      {/* When the state is off */}
+      <span className='off'>
+        <i>💡</i> I'm off!
+      </span>
+    </button>
+  );
+};
+
+export default LightSwitchButton;
