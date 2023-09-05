9c3262d70c57f5c262a5ce338160a057c106d28b
kazvee <kayvee123@gmail.com>
2023-09-04 18:19:14 -0600

    Implement useState on TextRepeaterButton component

--- src/components/TextRepeaterButton.js
+++ src/components/TextRepeaterButton.js
@@ -1,9 +1,14 @@
-const TextRepeaterButton = () => {
-  return (
-    <button className='TextRepeaterButton'>
-      <span>I like this text! 🤩</span>
-    </button>
-  );
+import { useState } from 'react';
+
+const TextRepeaterButton = (props) => {
+  const [repetitions, setRepetitions] = useState(1);
+  const textArray = [];
+
+  for (let i = 0; i < repetitions; i++) {
+    textArray.push(<span key={i}>I like this text! 🤩 </span>);
+  }
+
+  return <button className='TextRepeaterButton'>{textArray}</button>;
 };
 
 export default TextRepeaterButton;
