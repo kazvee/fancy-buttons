30402796f96a0f4e4af6fb6586866b34b8ed3820
kazvee <kayvee123@gmail.com>
2023-09-05 10:23:42 -0600

    Add click event handler to update TextRepeaterButton component state

--- src/components/TextRepeaterButton.js
+++ src/components/TextRepeaterButton.js
@@ -2,13 +2,20 @@ import { useState } from 'react';
 
 const TextRepeaterButton = (props) => {
   const [repetitions, setRepetitions] = useState(1);
+
   const textArray = [];
 
   for (let i = 0; i < repetitions; i++) {
     textArray.push(<span key={i}>I like this text! 🤩 </span>);
   }
 
-  return <button className='TextRepeaterButton'>{textArray}</button>;
+  const handleClick = () => setRepetitions(repetitions + 1);
+
+  return (
+    <button onClick={handleClick} className='TextRepeaterButton'>
+      {textArray}
+    </button>
+  );
 };
 
 export default TextRepeaterButton;
