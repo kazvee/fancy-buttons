ff6aacabc15d0c75754c9683bea1705117b939d0
kazvee <kayvee123@gmail.com>
2023-09-05 09:36:47 -0600

    Add click event handler to update LightSwitchButton component state

--- src/components/LightSwitchButton.js
+++ src/components/LightSwitchButton.js
@@ -3,8 +3,10 @@ import { useState } from 'react';
 const LightSwitchButton = (props) => {
   const [light, setLight] = useState('off');
 
+  const handleClick = () => setLight(light === 'on' ? 'off' : 'on')
+
   return (
-    <button className='LightSwitchButton'>
+    <button onClick={handleClick} className='LightSwitchButton'>
       {light === 'on' && <span><i>💡</i> I'm on!</span>}
       {light === 'off' && <span className='off'><i>💡</i> I'm off!</span>}
     </button>
