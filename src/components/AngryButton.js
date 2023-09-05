a85a1619944b4d9fca0457198a0a12f39a70a9f5
kazvee <kayvee123@gmail.com>
2023-09-05 09:13:46 -0600

    Add click event handler to update the state of AngryButton component

--- src/components/AngryButton.js
+++ src/components/AngryButton.js
@@ -3,8 +3,16 @@ import { useState } from 'react';
 const AngryButton = (props) => {
   const [anger, setAnger] = useState(0);
 
+  const handleClick = (amount) => {
+    if (anger < 1) {
+      setAnger(anger + amount);
+    } else {
+      setAnger(0);
+    }
+  };
+
   return (
-    <button style={{ backgroundColor: `rgba(255,0,0,${anger})` }} className='AngryButton'>
+    <button style={{ backgroundColor: `rgba(255,0,0,${anger})` }} onClick={() => handleClick(0.1)} className='AngryButton'>
       {anger < 1 && <span>Don't click me too much! </span>}
       {anger > 1 && <span>ANGRY MODE: ACTIVATED 😠</span>}
     </button>
